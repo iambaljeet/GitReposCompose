063f4865c60d058b7d1c00626e7f47e4fad69325
Baljeet <ibaljeet@outlook.com>
2020-10-03 13:35:59 +0530

    "Added UI for Git List"

---
+++ app/src/main/java/com/app/gitreposcompose/Components.kt
@@ -0,0 +1,56 @@
+package com.app.gitreposcompose
+
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.gitreposcompose.model.RepositoriesModel
+import dev.chrisbanes.accompanist.picasso.PicassoImage
+
+@Composable
+fun ReposListItem(repositoriesModelItem: RepositoriesModel.RepositoriesModelItem) {
+    Card(
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier.fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(10.dp)
+    ) {
+        Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                        .padding(10.dp)
+        ) {
+            Row(
+                    verticalAlignment = Alignment.CenterVertically
+            ) {
+                PicassoImage(
+                        data = repositoriesModelItem.owner?.avatarUrl.toString(),
+                        fadeIn = true,
+                        modifier = Modifier.preferredSize(size = 20.dp)
+                )
+                Spacer(modifier = Modifier.preferredSize(height = 0.dp, width = 5.dp))
+                Text(text = repositoriesModelItem.fullName?.substringBefore(delimiter = "/").toString(), fontSize = 16.sp)
+            }
+            Spacer(modifier = Modifier.preferredHeight(5.dp))
+            Text(text = repositoriesModelItem.name.toString(), fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.preferredHeight(5.dp))
+            Text(text = repositoriesModelItem.description.toString(), fontSize = 14.sp)
+        }
+    }
+}
+
+@Composable
+fun ReposList(repositoriesModel: RepositoriesModel?) {
+    ScrollableColumn {
+        repositoriesModel?.forEach { repositoriesModelItem ->
+            ReposListItem(repositoriesModelItem = repositoriesModelItem)
+        }
+    }
+}
--- app/src/main/java/com/app/gitreposcompose/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/gitreposcompose/ui/main/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
+import com.app.gitreposcompose.ReposList
 import com.app.gitreposcompose.model.ResultData
 import com.app.gitreposcompose.theming.GitReposComposeTheme
 import com.app.gitreposcompose.ui.main.viewmodel.MainViewModel
@@ -24,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             GitReposComposeTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    mainViewModel.getRepositoriesList("")
+                    mainViewModel.getRepositoriesList("1")
                     ReposData(mainViewModel)
                 }
             }
@@ -36,32 +41,25 @@ class MainActivity : AppCompatActivity() {
 fun ReposData(mainViewModel: MainViewModel) {
     val dataState = mainViewModel.repositoriesListLiveData.observeAsState()
 
-    when(val resultData = dataState.value) {
-        is ResultData.Loading -> {
-            Text(text = "Loading Data")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        when (val resultData = dataState.value) {
+            is ResultData.Loading -> {
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            is ResultData.Success -> {
+                val repositoriesModel = resultData.data
+                ReposList(repositoriesModel = repositoriesModel)
+            }
+            is ResultData.Failed -> {
+                Text(text = "Failed to load data.")
+            }
+            is ResultData.Exception -> {
+                Text(text = "Something went wrong. Please try again!")
+            }
         }
-        is ResultData.Success -> {
-            val repositoriesModel = resultData.data
-            Text(text = "Data: $repositoriesModel")
-        }
-        is ResultData.Failed -> {
-            Text(text = "Failed")
-        }
-        is ResultData.Exception -> {
-            Text(text = "Something went wrong Please try again!")
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GitReposComposeTheme {
-        Greeting("Android")
     }
 }
